5463186a33ba308ec51806f103172934b5db77ce
darealdevelopmaster <darealdevelopmaster@gmail.com>
2024-07-18 00:27:38 +0300

    finish

--- udacity.py
+++ udacity.py
@@ -1,100 +1,143 @@
-
 import time
 import random
+from typing import Tuple
+import sys, signal
+
+
+score = 0
+
+
+def exit():
+    print("\nSee you next time", end="")
+    print(f"\033[0m", end="")
+    sys.exit(0)
+
+
+@lambda f: signal.signal(signal.SIGINT, f)
+def signal_handler(*args):
+    exit()
+
+
+def custom_print(data: str, sep: float=0.0125, color: Tuple[int, int, int]=(198, 47, 222), safe: bool=True, *args, **kwargs):
+    try:
+        global COLOR
+        color = COLOR
+    except NameError:
+        color = color
+
+    r, g, b = color
+    print(f"\033[38;2;{r};{g};{b}m", end="")
+    for s in data:
+        print(s, flush=True, end="")
+        time.sleep(sep)
+    if safe:
+        print()
+    return color
+
+COLOR = custom_print("", safe=False)
+
+
+def custom_input(data):
+    custom_print(data, safe=False)
+    return input(">> ")
+
+
 def choice1():
-       score = 10
-       #if player score is 0 he loses
-       print("You find a monster infront of your face")
-       print("What will you do?")
-       time.sleep(2)
-       print("Enter 1 to run away from him")
-       time.sleep(2)
-       print("Enter 2 to beat him with your tool")
-       user_input=input("Please enter 1 or 2:")
-       while True:
-           if user_input=="1" or user_input=="2":
-               break
-           user_input=input("Please enter 1 or 2:")
-       if user_input=="1":
-           print("You were caught by the monster")
-           time.sleep(2)
-           print("You lost!")
-           
-           time.sleep(2)
-       else:
-           print("You beat him , he lost consciousness")
-           score += 50
-           #you earned 50 points
-           time.sleep(2)
-           print("You won!")
-       play_again()    
-def choice2():
-    print("You went inside the grave yard and found a magical wand")
-    time.sleep(2)
-    print("You decide to take it to help you")
-    time.sleep(2)
-    colors = ["red", "blue", "cyan", "purple", "yellow", "orange"]
-    random.choice(colors)
-    print("Your wand color is:", random.choice(colors))
-    time.sleep(2)
-    print("You found a ghost")
-    time.sleep(2)
-    print("Enter 1 to hide from him")
-    time.sleep(2)
-    print("Enter 2 cast a spell")
-    user_input=input("Please enter 1 or 2:")
-    while True:
-         if user_input=="1" or user_input=="2":
-             break
-         user_input=input("Please enter 1 or 2:")
+    global score
+    print(score, "score", flush=True)
+
+    custom_print("You find a monster infront of your face")
+    custom_print("What will you do?")
+    custom_print("Enter 1 to run away from him")
+    custom_print("Enter 2 to beat him with your tool")
+
+    while (user_input:=custom_input("Please enter 1 or 2:")) not in ("1", "2"): pass
     if user_input=="1":
-        print("The ghost kept searching, he found you")
-        time.sleep(2)
-        print("You lost!")
-        time.sleep(2)
+        custom_print("You were caught by the monster")
+        custom_print(f"You lost!")
+
     else:
-        print("You cast a spell on him , he disappeared")
-        time.sleep(2)
-        print("You won!")
+        custom_print("You beat him , he lost consciousness")
+        score += 50
+
+        custom_print("You won!")
     play_again()
+
+
+def choice2():
+    global COLOR, score
+    custom_print("You went inside the grave yard and found a magical wand")
+    custom_print("You decide to take it to help you")
+    colors = {
+        'red': (255, 0, 0),
+        'blue': (0, 0, 255),
+        'cyan': (0, 255, 255),
+        'purple': (128, 0, 128),
+        'yellow': (255, 255, 0),
+        'orange': (255, 165, 0)
+    }
+    color = random.choice(list(colors.keys()))
+    r, g, b = colors[color]
+    custom_print("")
+    custom_print(f"Your wand color is:", safe=False)
+    print(f"\033[38;2;{r};{g};{b}m", color, "\033[0m", end="", flush=True)
+    custom_print("You found a ghost")
+    custom_print("Enter 1 to hide from him")
+    custom_print("Enter 2 cast a spell")
+
+    while (user_input:=custom_input("Please enter 1 or 2:")) not in ("1", "2"): pass
+
+    if user_input=="1":
+        custom_print("The ghost kept searching, he found you")
+
+        custom_print("You lost!")
+
+    else:
+        custom_print("You cast a spell on him , he disappeared")
+
+        score += 100
+        custom_print("You won!")
+    play_again()
+
+
 def play_again():
-    choice = input("Would you like to play again?")
-    while choice not in ["yes", "no"]:
-        choice = input("Would you like to play again?")
+    global score
+    custom_print(f"With a score of {score}")
+    while (choice:=custom_input("Would you like to play again?")) not in ["yes", "no"]: pass
     if choice == "yes":
         start()
     elif choice == "no":
-        print("Thanks for playing")
+        custom_print("Thanks for playing")
+        exit()
+
+
 def start():
-    print("Welcome to the enchanted forest, a place of mystery and wonder, you are surrounded by towering trees, their branches reaching towards the sky.")
-    time.sleep(2)
-    print("But be warned, for this forest holds secrets and challenges. Hidden treasures, mythical creatures,Will you uncover the forest's mysteries?")
-    time.sleep(2)
-    print("To your right there is an abandoned house")
-    time.sleep(2)
-    print("Infront of you there is a grave yard")
-    score += 100
-    #your intial score is 100 points
-    time.sleep(2)
-    print("In your hand you have a sharp tool")
+    global score
+    custom_print("Welcome to the enchanted forest,\n\
+a place of mystery and wonder, you are surrounded by towering trees,\n\
+their branches reaching towards the sky.")
+    custom_print("But be warned, for this forest holds secrets and challenges. Hidden treasures, mythical creatures,Will you uncover the forest's mysteries?")
+    custom_print("To your right there is an abandoned house")
+    custom_print("Infront of you there is a grave yard")
+
+    custom_print("In your hand you have a sharp tool")
     tools = ["Axe", "Knife", "Peeler", "Saw", "Sharp Scissors"]
-    random.choice(tools)
-    print("Your sharp tool is:" ,random.choice(tools) )
-    time.sleep(2)
-    print("Enter 1 to knock on the door of the house.")
-    time.sleep(2)
-    print("Enter 2 to enter the grave yard.")
-    time.sleep(2)
-    print("What would you like to do?")
-    user_input=("Please enter 1 or 2:")
-    while True:
-        if user_input=="1" or user_input=="2":
-            break
-        user_input=input("Please enter 1 or 2:")
+    custom_print(f"Your sharp tool is: {random.choice(tools)}")
+    custom_print("Enter 1 to knock on the door of the house.")
+    custom_print("Enter 2 to enter the grave yard.")
+    custom_print("What would you like to do?")
+    choices = ("1", "2")
+    while (user_input := custom_input("Please enter 1 or 2:")) not in choices: pass
     if user_input=="1":
         choice1()
     else:
         choice2()
-start()    
-        
-        
+
+
+@lambda main: main()
+def main():
+    try:
+        start()
+    except Exception as e:
+        print(f"Sorry, I messed up in:\n{e.with_traceback(e.args)}")
+        exit()
